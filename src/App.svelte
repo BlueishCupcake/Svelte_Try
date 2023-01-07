1b882e7306a0cc292fcc5c670f3bc9ad09ed28ff
Castellian <sophiequines@gmail.com>
2023-01-07 18:58:59 -0300

    feat: starting the project

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+
+  let hello = `
+╭╮╱╭╮╱╱╭╮╭╮╱╱╱╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━━━╮╱╱╭╮
+┃┃╱┃┃╱╱┃┃┃┃╱╱╱┃╭━╮┃╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃╭━╮┃╱╭╯╰╮
+┃╰━╯┣━━┫┃┃┃╭━━┫┃╱╰╋╮╭┳━┳┳━━┳╮╭┳━━┫┃╱╰╋━┻╮╭╯
+┃╭━╮┃┃━┫┃┃┃┃╭╮┃┃╱╭┫┃┃┃╭╋┫╭╮┃┃┃┃━━┫┃╱╭┫╭╮┃┃
+┃┃╱┃┃┃━┫╰┫╰┫╰╯┃╰━╯┃╰╯┃┃┃┃╰╯┃╰╯┣━━┃╰━╯┃╭╮┃╰╮
+╰╯╱╰┻━━┻━┻━┻━━┻━━━┻━━┻╯╰┻━━┻━━┻━━┻━━━┻╯╰┻━╯`
+
+  console.log(hello)
+
+  const handleClick = () => {
+    console.log('test')
+  }
+</script>
+
+<main>
+
+  <section class="main-section">
+
+    <h1>Hello World!</h1>
+  </section>
+</main>
+
+<style>
+
+  .main-section {
+    display: flex;
+    justify-content: center;
+    place-content: center;
+   }
+</style>
